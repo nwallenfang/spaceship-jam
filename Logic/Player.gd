9e509313d3c37338905c7c4f33904b2371500071
nwallenfang <nils@wallenfang.de>
2023-12-05 20:46:21 +0100

    add fantastic smoothing plugin

--- Logic/Player.gd
+++ Logic/Player.gd
@@ -7,6 +7,12 @@ extends CharacterBody3D
 @export var air_friction := 0.05
 
 
+var accumulated_movement: Vector3 = Vector3.ZERO
+
+
+func _ready():
+	$DebugMesh.visible = false
+
 func accelerate(_old_velocity: Vector3, direction: Vector3, delta: float) -> Vector3:
 	# Using only the horizontal velocity, interpolate towards the input.
 	# apply ground friction if on floor
@@ -18,10 +24,15 @@ func accelerate(_old_velocity: Vector3, direction: Vector3, delta: float) -> Vec
 	
 	return velocity
 
+func _process(_delta: float):
+	pass
+	# TODO accelerate:
+	#accelerate
+
 
 func _physics_process(delta):
 	var movement = Vector3()
-
+	
 	if Input.is_action_pressed("move_left"):
 		movement.x -= 1.0
 	if Input.is_action_pressed("move_right"):
@@ -30,6 +41,7 @@ func _physics_process(delta):
 		movement.z -= 1.0
 	if Input.is_action_pressed("move_backwards"):
 		movement.z += 1.0
+	# TODO accumulate inputs in physics / input
 
 	var yaw_rotation = Basis(Vector3.UP, rotation.y)
 	movement = (yaw_rotation * movement).normalized() * speed * delta

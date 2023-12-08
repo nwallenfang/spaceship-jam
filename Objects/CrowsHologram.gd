5db8ce3289c4a193bacba89b12b8fe41025da316
Nils Wallenfang <nils@wallenfang.de>
2023-12-08 15:06:54 +0100

    better slerp speed

--- Objects/CrowsHologram.gd
+++ Objects/CrowsHologram.gd
@@ -2,11 +2,13 @@ class_name CrowsHologram extends MeshInstance3D
 
 @export var camera: Camera3D
 @export var target_node: Node3D
-@export var interpolation_speed: float = 0.9
+@export var interpolation_speed: float = 1.5
+@export var minimum_speed: float = 0.8
 @onready var sphere_radius: float = target_node.global_position.distance_to(camera.global_position)
 
 func _process(delta):
 	var camera_pos = camera.global_transform.origin
+	# positions relative to center of sphere (CrowsCamera)
 	var hologram_pos_rel = global_transform.origin - camera_pos
 	var target_pos_rel = target_node.global_transform.origin - camera_pos
 
@@ -14,8 +16,13 @@ func _process(delta):
 	hologram_pos_rel = hologram_pos_rel.normalized() * sphere_radius
 	target_pos_rel = target_pos_rel.normalized() * sphere_radius
 
+	var distance_to_target = hologram_pos_rel.distance_to(target_pos_rel)
+	var factor = delta * interpolation_speed * (distance_to_target / sphere_radius + minimum_speed)  # Ensuring a minimum factor
+	factor = clamp(factor, 0.0, 1.0)  # Clamp between 0 and 1
+
 	# slerp the position
-	var new_pos_relative = hologram_pos_rel.slerp(target_pos_rel, interpolation_speed * delta)
+	var new_pos_relative = hologram_pos_rel.slerp(target_pos_rel, factor)
+	# relative to absolute position
 	global_transform.origin = camera_pos + new_pos_relative
 	
 	# rotate Hologram so it's perpendicular to the Camera to it
